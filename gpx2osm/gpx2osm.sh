5e529a79df7a009cb2f086f1f1b976308523470a
Forest Gregg <fgregg@uchicago.edu>
2012-02-10 00:46:27 -0600

    changed the sleep time between map matchings

--- gpx2osm/gpx2osm.sh
+++ gpx2osm/gpx2osm.sh
@@ -3,5 +3,5 @@ do
     mono MatchGPX2OSM.exe --osm=chicago-r.osm --gpx=../gpx --output=../osm
     scp ../gpx/* fgregg@bunkum.us:www/gpx
     scp ../osm/* fgregg@bunkum.us:www/osm
-    sleep 10
+    sleep 300
 done
