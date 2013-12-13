44c62827a7f33792af5ea53fa647c4d6d52910c9
Derek Eder <derek.eder@gmail.com>
2013-12-13 15:14:25 -0600

    properly looping forever and doing datechecks for incremental inserts

--- osm2cartodb/osm_to_cartodb.py
+++ osm2cartodb/osm_to_cartodb.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import datetime
+import time
 
 from cartodb_settings import CARTODB_SETTINGS
 from cartodb import CartoDBAPIKey, CartoDBException
@@ -9,10 +10,14 @@ import xml.etree.ElementTree as ET
 def get_latest_insert(plow_id, carto):
   latest = datetime.datetime(1990, 1, 1)
   try:
-    query = carto.sql("select datestamp from %s where id = '%s'" % (CARTODB_SETTINGS['table'], plow_id))
+    query = carto.sql("select date_stamp from %s where id = '%s' ORDER BY date_stamp DESC LIMIT 1" % (CARTODB_SETTINGS['table'], plow_id))
+    
+    if len(query['rows']):
+      row = query['rows'][0]
 
-    if 'datestamp' in query.keys():
-      latest = query['datestamp']
+      if 'date_stamp' in row.keys():
+        latest = datetime.datetime.strptime(row['date_stamp'], "%Y-%m-%d %H:%M:%S")
+
     return latest
 
   except CartoDBException as e:
@@ -37,7 +42,7 @@ def add_to_insert_batch(plow_id, datestamp, the_geom, carto, insert_batch, batch
   return insert_batch
 
 def commit_insert_batch(carto, insert_batch):
-  insert_sql = "INSERT INTO %s (id, datestamp, the_geom) VALUES " % (CARTODB_SETTINGS['table'])
+  insert_sql = "INSERT INTO %s (id, date_stamp, the_geom) VALUES " % (CARTODB_SETTINGS['table'])
 
   for item in insert_batch:
     insert_sql += " ('%s', '%s', ST_GeomFromText('LINESTRING(%s)', 4326))," % (item[0], item[1], item[2])
@@ -66,49 +71,53 @@ carto = CartoDBAPIKey(API_KEY, cartodb_domain)
 
 # clear_out_table(carto)
 
-# while True:
+while True:
 
-insert_batch = []
-batch_size = 1000
-insert_count = 0
-osm_files = [ f for f in listdir('../osm') if isfile(join('../osm',f)) and '.osm' in f]
+  insert_batch = []
+  batch_size = 1000
+  insert_count = 0
+  osm_files = [ f for f in listdir('../osm') if isfile(join('../osm',f)) and '.osm' in f]
 
-for osm_file in osm_files:
+  print 'OSM files:'
+  print osm_files
 
-  plow_id = osm_file.split("_")[0]
-  datestamp = ''
-  the_geom = []
+  for osm_file in osm_files:
 
-  print 'importing', plow_id
+    plow_id = osm_file.split("_")[0]
+    datestamp = ''
+    the_geom = []
 
-  latest_insert = get_latest_insert(plow_id, carto)
-  current_segment_datestamp = datetime.datetime(1990, 1, 2)
-  # print 'latest_insert', latest_insert
+    latest_insert = get_latest_insert(plow_id, carto)
+    current_segment_datestamp = datetime.datetime(1990, 1, 2)
 
-  # read OSM file
-  tree = ET.parse("../osm/%s" % osm_file)
-  for node in tree.getroot().iter('node'):
-    # print current_segment_datestamp
-    # print node.attrib
-    for child in node:
-      # print child.attrib
-      if child.attrib['k'] == 'time':
-        if len(the_geom) > 0 and current_segment_datestamp > latest_insert:
-          the_geom.append([node.attrib['lat'], node.attrib['lon']])
-          insert_batch = add_to_insert_batch(plow_id, datestamp, the_geom, carto, insert_batch, batch_size)
-          insert_count = insert_count + 1
+    # read OSM file
+    tree = ET.parse("../osm/%s" % osm_file)
+    for node in tree.getroot().iter('node'):
+      # print current_segment_datestamp
+      # print node.attrib
+      for child in node:
+        # print child.attrib
+        if child.attrib['k'] == 'time':
+          if len(the_geom) > 0 and current_segment_datestamp > latest_insert:
+            # print current_segment_datestamp, '>', latest_insert
+            the_geom.append([node.attrib['lat'], node.attrib['lon']])
+            insert_batch = add_to_insert_batch(plow_id, datestamp, the_geom, carto, insert_batch, batch_size)
+            insert_count = insert_count + 1
 
-          if insert_count % batch_size == 0:
-            commit_insert_batch(carto, insert_batch)
-            insert_batch = []
-            print "inserted %s so far" % insert_count;
-        the_geom = []
-        current_segment_datestamp = datetime.datetime.strptime(child.attrib['v'], "%m/%d/%Y %I:%M:%S %p")
+            if insert_count % batch_size == 0:
+              commit_insert_batch(carto, insert_batch)
+              insert_batch = []
+              print "inserted %s so far" % insert_count;
+          the_geom = []
+          current_segment_datestamp = datetime.datetime.strptime(child.attrib['v'], "%m/%d/%Y %I:%M:%S %p")
 
-      datestamp = current_segment_datestamp
-      the_geom.append([node.attrib['lat'], node.attrib['lon']])
+        datestamp = current_segment_datestamp
+        the_geom.append([node.attrib['lat'], node.attrib['lon']])
 
-# do one final insert for the remainder
-if len(insert_batch) > 0:
-  commit_insert_batch(carto, insert_batch)
-  print "inserted the remaining %s" % len(insert_batch);
+  # do one final insert for the remainder
+  if len(insert_batch) > 0:
+    commit_insert_batch(carto, insert_batch)
+    print "inserted the remaining %s" % len(insert_batch);
+
+  print 'sleeping for 5 min'
+  time.sleep(300)
