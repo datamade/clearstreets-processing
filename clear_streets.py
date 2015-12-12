a1902945f087b7bd959e37822a202dc9c5505be9
Eric van Zanten <eric.vanzanten@gmail.com>
2015-12-12 14:40:38 -0600

    Added backup task to main script

--- clear_streets.py
+++ clear_streets.py
@@ -17,6 +17,9 @@ if __name__ == "__main__":
     
     parser.add_argument('--write_cartodb', action='store_true',
                    help='Write slurped traces to CartoDB')
+    
+    parser.add_argument('--backup', action='store_true',
+                   help='Backup database contents to S3')
 
     args = parser.parse_args()
     
@@ -34,3 +37,42 @@ if __name__ == "__main__":
         while True:
             tracer.run()
             time.sleep(10)
+
+    if args.backup:
+        from datetime import datetime
+        import sqlalchemy as sa
+        
+        import boto
+        from boto.s3.key import Key
+        
+        from processors.config import DB_CONN, AWS_KEY, AWS_SECRET, S3_BUCKET
+        
+        engine = sa.create_engine(DB_CONN)
+
+        conn = engine.raw_connection()
+
+        s3conn = boto.connect_s3(AWS_KEY, AWS_SECRET)
+        bucket = s3conn.get_bucket(S3_BUCKET)
+
+        for table in ['route_points', 'assets']:
+            copy = ''' 
+                COPY (SELECT * FROM {table})
+                TO STDOUT WITH CSV HEADER DELIMITER ','
+            '''.format(table=table)
+            
+            now = datetime.now().strftime('%m-%d-%Y_%H:%M')
+            
+            fname = 'backups/{now}_{table}.csv'.format(now=now, 
+                                                       table=table)
+            
+            with open(fname, 'w') as f:
+                curs = conn.cursor()
+                curs.copy_expert(copy, f)
+            
+            key = Key(bucket)
+            key.key = fname
+            key.set_contents_from_filename(fname)
+            key.set_acl('public-read')
+
+        conn.close()
+        s3conn.close()
