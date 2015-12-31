339ce079ebb5b8076e0f6f41b2eb8f71d9796cbc
Eric van Zanten <eric.vanzanten@gmail.com>
2015-12-31 12:17:44 -0600

    Trying a different path

--- startup_tasks.sh
+++ startup_tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://s3.amazonaws.com/metro-extracts.mapzen.com/chicago_illinois.osm.pbf
-osrm-extract chicago_illinois.osm.pbf
-osrm-prepare chicago_illinois.osrm
-osrm-routed chicago_illinois.osrm
+$HOME/osrm-backend/build/osrm-extract chicago_illinois.osm.pbf
+$HOME/osrm-backend/build/osrm-prepare chicago_illinois.osrm
+$HOME/osrm-backend/build/osrm-routed chicago_illinois.osrm
