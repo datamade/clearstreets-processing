9f8ee27e347ca7eb66d86fbe8548bda5fbd1b9a0
Eric van Zanten <eric.vanzanten@gmail.com>
2015-12-31 11:59:08 -0600

    Add OSRM startup script

---
+++ startup_tasks.sh
@@ -0,0 +1,4 @@
+wget https://s3.amazonaws.com/metro-extracts.mapzen.com/chicago_illinois.osm.pbf
+osrm-extract chicago_illinois.pdf
+osrm-prepare chicago_illinois.osrm
+osrm-routed chicago_illinois.osrm
